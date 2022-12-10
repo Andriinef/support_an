120c1d50d746f017debeaa2cca4794398774d554
Andriinef <andrii.nefedov@nure.ua>
2022-12-11 01:08:09 +0200

    correct flake8

--- src/config/settings.py
+++ src/config/settings.py
@@ -44,7 +44,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-    "support.apps.SupportConfig" # new apps
+    "support.apps.SupportConfig",  # new apps
 ]
 
 MIDDLEWARE = [
