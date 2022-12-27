4196a1a836392ef839bd04f3926653153536f5e8
Andriinef <andrii.nefedov@nure.ua>
2022-12-27 11:32:49 +0200

    exchange_rates corect

--- src/exchange_rates/api.py
+++ src/exchange_rates/api.py
@@ -4,24 +4,25 @@ from django.http import JsonResponse
 
 from exchange_rates.services import AlphavantageClient, AlphavantageResponse
 
+# def get_price(request):
+#     client = AlphavantageClient()
+#     return JsonResponse(
+#         AlphavantageResponse(**client.get_user_currency("USD", "UAH")).dict()
+#     )
 
-def get_price(request):
-    client = AlphavantageClient()
-    return JsonResponse(
-        AlphavantageResponse(**client.get_user_currency("USD", "UAH")).dict()
+
+def get_price(request) -> JsonResponse:
+    if request.method == "POST":
+        body: dict = json.loads(request.body)
+        from_currency: str = body["from"]
+        to_currency: str = body["to"]
+    elif request.method == "GET":
+        from_currency: str = "USD"
+        to_currency: str = "UAH"
+    responce = AlphavantageResponse(
+        **AlphavantageClient().get_user_currency(from_currency, to_currency)
     )
-
-
-def get_post_user(request):
-    body = json.loads(request.body)
-    from_currency = body["from"]
-    to_currency = body["to"]
-    client = AlphavantageClient()
-    return JsonResponse(
-        AlphavantageResponse(
-            **client.get_user_currency(from_currency, to_currency)
-        ).dict()
-    )
+    return JsonResponse(responce.dict())
 
 
 # def get_post_user(request):
--- src/exchange_rates/urls.py
+++ src/exchange_rates/urls.py
@@ -1,8 +1,7 @@
 from django.urls import path
 
-from exchange_rates.api import get_post_user, get_price
+from exchange_rates.api import get_price
 
 urlpatterns = [
     path("price/", get_price, name="price"),
-    path("post_user/", get_post_user, name="get_post_user"),
 ]
