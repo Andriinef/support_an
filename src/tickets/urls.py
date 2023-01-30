05089538577ef61c7116288a86f8fd7baa2fb467
Andriinef <andrii.nefedov@nure.ua>
2023-01-30 19:17:15 +0200

    urls

--- src/tickets/urls.py
+++ src/tickets/urls.py
@@ -8,13 +8,13 @@ from tickets.api_views import (TicketAPISet, TicketCreateAPIView,
 
 router = DefaultRouter()
 router.register(r"tickets", TicketAPISet, basename="tickets")
-urlpatterns = router.urls
 
-# urlpatterns = [
-#     path("list/", TicketsListAPIView.as_view()),
-#     path("create/", TicketCreateAPIView.as_view()),
-#     path("listcreate/", TicketListCreateAPIView.as_view()),
-#     path("retrieve/<slug:slug>/", TicketRetrieveAPIView.as_view()),
-#     path("retrieveupdate/<slug:slug>/", TicketRetrieveUpdateAPIView.as_view()),
-#     path("retrievedestroy/<slug:slug>/", TicketRetrieveDestroyAPIView.as_view()),
-# ]
+
+urlpatterns = [
+    path("list/", TicketsListAPIView.as_view()),
+    path("create/", TicketCreateAPIView.as_view()),
+    path("listcreate/", TicketListCreateAPIView.as_view()),
+    path("retrieve/<slug:slug>/", TicketRetrieveAPIView.as_view()),
+    path("retrieveupdate/<slug:slug>/", TicketRetrieveUpdateAPIView.as_view()),
+    path("retrievedestroy/<slug:slug>/", TicketRetrieveDestroyAPIView.as_view()),
+] + router.urls
